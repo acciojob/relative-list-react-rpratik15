72558d1db8f3b729de646b2b9d56a939f042f1bb
Pratik Raval <rpratik15@gmail.com>
2023-01-06 18:17:23 +0530

    update1

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Relatives from './Relatives'
 
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+          <Relatives/>
     </div>
   )
 }
---
+++ src/components/Relatives.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Relatives() {
+  return (
+    <div>
+        <ul>
+            <li key="relativeListItem1">Rohan Desai</li>
+            <li key="relativeListItem2">Nitin Jha</li>
+            <li key="relativeListItem3">Pawan Kumar</li>
+            <li key="relativeListItem4">Aanil Joshi</li>
+            <li key="relativeListItem5">Ketan Jadhav</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Relatives
