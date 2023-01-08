71f78c1ead29da7127e8c048d67a56aad8ef03a9
Pratik Raval <rpratik15@gmail.com>
2023-01-08 16:43:55 +0530

    update4

--- src/components/Relatives.js
+++ src/components/Relatives.js
@@ -2,13 +2,18 @@ import React from 'react'
 
 function Relatives() {
   const list=["Rohan Desai","Nitin Jha","Pawan Kumar","Aanil Joshi","Ketan Jadhav"]
-  const listItem=list.map((element,index)=>{
+  const listItem=list.map((element,index)=>
 
-    <li key={`relativeListItem${index}`}>{element}</li>
+    <li key={`relativeListItem${index.toString()}`}>{element}</li>
 
-  })
+  );
+
+  
+
+  
   return (
     <div>
+      
         <ol key={'relativeList'}>
           {listItem}
         </ol>
